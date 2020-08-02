79cc8a67bf9385d5fc68d2862ae6a38affcf412a
rongwei <rongwei_alisa@hotmail.com>
2020-08-02 23:16:12 +0800

    feat: 冒泡排序注释

--- 算法&数据结构/冒泡排序/sort.js
+++ 算法&数据结构/冒泡排序/sort.js
@@ -3,7 +3,7 @@
  * @Author: RongWei
  * @Date: 2020-08-02 23:05:46
  * @LastEditors: RongWei
- * @LastEditTime: 2020-08-02 23:12:06
+ * @LastEditTime: 2020-08-02 23:16:04
  */
 function sort(arr) {
     const len = arr.length;
@@ -12,6 +12,7 @@ function sort(arr) {
     for (let i = 0; i < len; i++) {
         // 提前退出循环的标识
         let sortAlready = true;
+        // 每一次排序都会将最大的元素交换至数组末尾，所以下一次排序只需要比较到len - i - 1的位置即可
         for (let j = 0; j < len - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j];
